06ad4b43986b646087356e4db55c944ac5cca46b
LianJordaan <lianjordaan30052005@gmail.com>
2023-10-04 23:30:15 +0200

    Remade movement check

--- src/main/java/hypersquare/hypersquare/plot/RestrictMovement.java
+++ src/main/java/hypersquare/hypersquare/plot/RestrictMovement.java
@@ -7,8 +7,6 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.persistence.PersistentDataType;
 
-import java.util.*;
-
 public class RestrictMovement {
 
     public static Location basic = null;
@@ -34,47 +32,81 @@ public class RestrictMovement {
             commonVars(player.getLocation());
             int plotID = Utilities.getPlotID(player.getWorld());
             String plotType = player.getWorld().getPersistentDataContainer().get(new NamespacedKey(Hypersquare.getPlugin(Hypersquare.class),"plotType"), PersistentDataType.STRING);
-            List visitedLocations = new ArrayList();
 
             switch (plotType){
 
                 case "Basic" : {
                     if (!Utilities.locationWithin(player.getLocation(),commonStart,basic)) {
-                        Hypersquare.teleportFlagMap.put(player, false);
-                        visitedLocations.add(1);
-                        Utilities.moveRecursively(player,player.getLocation(),commonStart,basic);
+                        //Hypersquare.teleportFlagMap.put(player, false);
+                        //Utilities.moveRecursively(player,player.getLocation(),commonStart,basic);
+                        player.teleport(new Location(player.getWorld(),
+                                Math.max(player.getLocation().getX(), commonStart.getX()),
+                                player.getLocation().getY(),
+                                Math.max(player.getLocation().getZ(), commonStart.getZ())));
+                        player.teleport(new Location(player.getWorld(),
+                                Math.min(player.getLocation().getX(), basic.getX()),
+                                player.getLocation().getY(),
+                                Math.min(player.getLocation().getZ(), basic.getZ())));
                     }
                     break;
                 }
                 case "Large" : {
                     if (!Utilities.locationWithin(player.getLocation(),commonStart,large)) {
-                        Hypersquare.teleportFlagMap.put(player, false);
-                        visitedLocations.add(1);
-                        Utilities.moveRecursively(player,player.getLocation(),commonStart,large);
+                        //Hypersquare.teleportFlagMap.put(player, false);
+                        //Utilities.moveRecursively(player,player.getLocation(),commonStart,large);
+                        player.teleport(new Location(player.getWorld(),
+                                Math.max(player.getLocation().getX(), commonStart.getX()),
+                                player.getLocation().getY(),
+                                Math.max(player.getLocation().getZ(), commonStart.getZ())));
+                        player.teleport(new Location(player.getWorld(),
+                                Math.min(player.getLocation().getX(), large.getX()),
+                                player.getLocation().getY(),
+                                Math.min(player.getLocation().getZ(), large.getZ())));
                     }
                     break;
                 }
                 case "Massive" : {
                     if (!Utilities.locationWithin(player.getLocation(),commonStart,massive)) {
-                        Hypersquare.teleportFlagMap.put(player, false);
-                        visitedLocations.add(1);
-                        Utilities.moveRecursively(player,player.getLocation(),commonStart,massive);
+                        //Hypersquare.teleportFlagMap.put(player, false);
+                        //Utilities.moveRecursively(player,player.getLocation(),commonStart,massive);
+                        player.teleport(new Location(player.getWorld(),
+                                Math.max(player.getLocation().getX(), commonStart.getX()),
+                                player.getLocation().getY(),
+                                Math.max(player.getLocation().getZ(), commonStart.getZ())));
+                        player.teleport(new Location(player.getWorld(),
+                                Math.min(player.getLocation().getX(), massive.getX()),
+                                player.getLocation().getY(),
+                                Math.min(player.getLocation().getZ(), massive.getZ())));
                     }
                     break;
                 }
                 case "Huge" : {
                     if (!Utilities.locationWithin(player.getLocation(),commonStart,huge)) {
-                        Hypersquare.teleportFlagMap.put(player, false);
-                        visitedLocations.add(1);
-                        Utilities.moveRecursively(player,player.getLocation(),commonStart,huge);
+                        //Hypersquare.teleportFlagMap.put(player, false);
+                        //Utilities.moveRecursively(player,player.getLocation(),commonStart,huge);
+                        player.teleport(new Location(player.getWorld(),
+                                Math.max(player.getLocation().getX(), commonStart.getX()),
+                                player.getLocation().getY(),
+                                Math.max(player.getLocation().getZ(), commonStart.getZ())));
+                        player.teleport(new Location(player.getWorld(),
+                                Math.min(player.getLocation().getX(), huge.getX()),
+                                player.getLocation().getY(),
+                                Math.min(player.getLocation().getZ(), huge.getZ())));
                     }
                     break;
                 }
                 case "Gigantic" : {
                     if (!Utilities.locationWithin(player.getLocation(),commonStart,gigantic)) {
-                        Hypersquare.teleportFlagMap.put(player, false);
-                        visitedLocations.add(1);
-                        Utilities.moveRecursively(player,player.getLocation(),commonStart,gigantic);
+                        //Hypersquare.teleportFlagMap.put(player, false);
+                        //Utilities.moveRecursively(player,player.getLocation(),commonStart,gigantic);
+                        player.teleport(new Location(player.getWorld(),
+                                Math.max(player.getLocation().getX(), commonStart.getX()),
+                                player.getLocation().getY(),
+                                Math.max(player.getLocation().getZ(), commonStart.getZ())));
+                        player.teleport(new Location(player.getWorld(),
+                                Math.min(player.getLocation().getX(), gigantic.getX()),
+                                player.getLocation().getY(),
+                                Math.min(player.getLocation().getZ(), gigantic.getZ())));
                     }
                     break;
                 }
@@ -84,47 +116,81 @@ public class RestrictMovement {
             commonVars(player.getLocation());
             int plotID = Utilities.getPlotID(player.getWorld());
             String plotType = player.getWorld().getPersistentDataContainer().get(new NamespacedKey(Hypersquare.getPlugin(Hypersquare.class),"plotType"), PersistentDataType.STRING);
-            List visitedLocations = new ArrayList();
 
             switch (plotType){
 
                 case "Basic" : {
                     if (!Utilities.locationWithin(player.getLocation(),commonStart.clone().add(20,0,0),basic)) {
-                        Hypersquare.teleportFlagMap.put(player, false);
-                        visitedLocations.add(1);
-                        Utilities.moveRecursively(player,player.getLocation(),commonStart.clone().add(20,0,0),basic);
+                        //Hypersquare.teleportFlagMap.put(player, false);
+                        //Utilities.moveRecursively(player,player.getLocation(),commonStart.clone().add(20,0,0),basic);
+                        player.teleport(new Location(player.getWorld(),
+                                Math.max(player.getLocation().getX(), commonStart.clone().getX() + 20),
+                                player.getLocation().getY(),
+                                Math.max(player.getLocation().getZ(), commonStart.clone().getZ() + 20)));
+                        player.teleport(new Location(player.getWorld(),
+                                Math.min(player.getLocation().getX(), basic.getX()),
+                                player.getLocation().getY(),
+                                Math.min(player.getLocation().getZ(), basic.getZ())));
                     }
                     break;
                 }
                 case "Large" : {
                     if (!Utilities.locationWithin(player.getLocation(),commonStart.clone().add(20,0,0),large)) {
-                        Hypersquare.teleportFlagMap.put(player, false);
-                        visitedLocations.add(1);
-                        Utilities.moveRecursively(player,player.getLocation(),commonStart.clone().add(20,0,0),large);
+                        //Hypersquare.teleportFlagMap.put(player, false);
+                        //Utilities.moveRecursively(player,player.getLocation(),commonStart.clone().add(20,0,0),large);
+                        player.teleport(new Location(player.getWorld(),
+                                Math.max(player.getLocation().getX(), commonStart.clone().getX() + 20),
+                                player.getLocation().getY(),
+                                Math.max(player.getLocation().getZ(), commonStart.clone().getZ() + 20)));
+                        player.teleport(new Location(player.getWorld(),
+                                Math.min(player.getLocation().getX(), large.getX()),
+                                player.getLocation().getY(),
+                                Math.min(player.getLocation().getZ(), large.getZ())));
                     }
                     break;
                 }
                 case "Massive" : {
                     if (!Utilities.locationWithin(player.getLocation(),commonStart.clone().add(20,0,0),massive)) {
-                        Hypersquare.teleportFlagMap.put(player, false);
-                        visitedLocations.add(1);
-                        Utilities.moveRecursively(player,player.getLocation(),commonStart.clone().add(20,0,0),massive);
+                        //Hypersquare.teleportFlagMap.put(player, false);
+                        //Utilities.moveRecursively(player,player.getLocation(),commonStart.clone().add(20,0,0),massive);
+                        player.teleport(new Location(player.getWorld(),
+                                Math.max(player.getLocation().getX(), commonStart.clone().getX() + 20),
+                                player.getLocation().getY(),
+                                Math.max(player.getLocation().getZ(), commonStart.clone().getZ() + 20)));
+                        player.teleport(new Location(player.getWorld(),
+                                Math.min(player.getLocation().getX(), massive.getX()),
+                                player.getLocation().getY(),
+                                Math.min(player.getLocation().getZ(), massive.getZ())));
                     }
                     break;
                 }
                 case "Huge" : {
                     if (!Utilities.locationWithin(player.getLocation(),commonStart.clone().add(20,0,0),huge)) {
-                        Hypersquare.teleportFlagMap.put(player, false);
-                        visitedLocations.add(1);
-                        Utilities.moveRecursively(player,player.getLocation(),commonStart.clone().add(20,0,0),huge);
+                        //Hypersquare.teleportFlagMap.put(player, false);
+                        //Utilities.moveRecursively(player,player.getLocation(),commonStart.clone().add(20,0,0),huge);
+                        player.teleport(new Location(player.getWorld(),
+                                Math.max(player.getLocation().getX(), commonStart.clone().getX() + 20),
+                                player.getLocation().getY(),
+                                Math.max(player.getLocation().getZ(), commonStart.clone().getZ() + 20)));
+                        player.teleport(new Location(player.getWorld(),
+                                Math.min(player.getLocation().getX(), huge.getX()),
+                                player.getLocation().getY(),
+                                Math.min(player.getLocation().getZ(), huge.getZ())));
                     }
                     break;
                 }
                 case "Gigantic" : {
                     if (!Utilities.locationWithin(player.getLocation(),commonStart.clone().add(20,0,0),gigantic)) {
-                        Hypersquare.teleportFlagMap.put(player, false);
-                        visitedLocations.add(1);
-                        Utilities.moveRecursively(player,player.getLocation(),commonStart.clone().add(20,0,0),gigantic);
+                        //Hypersquare.teleportFlagMap.put(player, false);
+                        //Utilities.moveRecursively(player,player.getLocation(),commonStart.clone().add(20,0,0),gigantic);
+                        player.teleport(new Location(player.getWorld(),
+                                Math.max(player.getLocation().getX(), commonStart.clone().getX() + 20),
+                                player.getLocation().getY(),
+                                Math.max(player.getLocation().getZ(), commonStart.clone().getZ() + 20)));
+                        player.teleport(new Location(player.getWorld(),
+                                Math.min(player.getLocation().getX(), gigantic.getX()),
+                                player.getLocation().getY(),
+                                Math.min(player.getLocation().getZ(), gigantic.getZ())));
                     }
                     break;
                 }

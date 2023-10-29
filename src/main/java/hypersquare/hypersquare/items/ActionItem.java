8df6abe9d55d84dbd0654e7c33a5b87f80824bb4
Chickensoup20 <87900686+Chickensoup20@users.noreply.github.com>
2023-10-29 22:17:19 +0200

    Fixes to Action Item Builder - Chicken

--- src/main/java/hypersquare/hypersquare/items/ActionItem.java
+++ src/main/java/hypersquare/hypersquare/items/ActionItem.java
@@ -9,6 +9,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static hypersquare.hypersquare.Hypersquare.mm;
@@ -20,6 +21,8 @@ public class ActionItem {
     Material material;
     String description;
     ActionArgument[] arguments;
+    List<String[]> additionalInfo = List.of();
+    boolean enchanted;
 
     public ActionItem setMaterial(Material material){
         this.material = material;
@@ -38,6 +41,19 @@ public class ActionItem {
         return this;
     }
 
+    public ActionItem addAdditionalInfo(String... info){
+        List<String[]> infoList = new ArrayList<>(additionalInfo);
+        info[0] = "<!italic><blue>⏵ <gray>" + info[0];
+        infoList.add(info);
+        this.additionalInfo = infoList;
+        return this;
+    }
+
+    public ActionItem setEnchanted(boolean enchanted) {
+        this.enchanted = enchanted;
+        return this;
+    }
+
     public ItemStack build(){
         actionItem = new ItemStack(material);
         ItemMeta meta = actionItem.getItemMeta();
@@ -51,23 +67,38 @@ public class ActionItem {
         }
 
         // Arguments
+
         StringBuilder builder = new StringBuilder();
         builder.append("%n<white>Chest Parameters:%n");
-        for (ActionArgument actionArgument : arguments){
-            builder.append(mm.serialize(actionArgument.getType().getName(actionArgument.type)));
-            builder.append(actionArgument.plural ? "(s)" : "");
-            builder.append(actionArgument.optional ? "*" : "");
-            builder.append(" <dark_gray>- <gray>").append(actionArgument.description).append("%n");
-        }
+        if (arguments != null) {
+            for (ActionArgument actionArgument : arguments) {
+                builder.append(mm.serialize(actionArgument.getType().getName(actionArgument.type)));
+                builder.append(actionArgument.plural ? "(s)" : "");
+                builder.append(actionArgument.optional ? "*" : "");
+                builder.append(" <dark_gray>- <gray>").append(actionArgument.description).append("%n");
+            }
+        } else
+            builder.append("<dark_gray>None");
         String builderString = String.valueOf(builder);
 
-        parts = builderString.split("%n");
+        String[] parts = builderString.split("%n");
         for (String part : parts) {
             lore.add(mm.deserialize("<!italic>" + part));
         }
 
-        //Name
-        meta.displayName(mm.deserialize("<!italic>" + name));
+        // Additional Info
+        if (additionalInfo != null) {
+            lore.add(Component.text(""));
+            lore.add(mm.deserialize("<!italic><blue>Additional info:"));
+            for (String[] info : additionalInfo) {
+                for (String text : info) {
+                    lore.add(mm.deserialize("<!italic><gray>" + text));
+                }
+            }
+        }
+
+        // Enchanted
+        if (enchanted) meta.addEnchant(Enchantment.LURE, 1, true);
 
         meta.lore(lore);
         actionItem.setItemMeta(meta);

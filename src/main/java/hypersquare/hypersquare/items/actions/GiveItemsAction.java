060ed6d6541e30c4732b9b4d8324b3761dd40493
Chickensoup20 <87900686+Chickensoup20@users.noreply.github.com>
2023-10-31 11:05:30 +0200

    a

---
+++ src/main/java/hypersquare/hypersquare/items/actions/GiveItemsAction.java
@@ -0,0 +1,77 @@
+package hypersquare.hypersquare.items.actions;
+
+import hypersquare.hypersquare.items.*;
+import org.bukkit.Material;
+import org.bukkit.block.Chest;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+
+import static hypersquare.hypersquare.Hypersquare.mm;
+
+public class GiveItemsAction implements Action {
+
+    public ItemStack item() {
+        return new ActionItem()
+            .setMaterial(Material.CHEST)
+            .setName("<green>Give Items")
+            .setDescription("Gives the player all of the", "items in the chest")
+            .setArguments(
+                    new ActionArgument()
+                            .setType(DisplayValue.ITEM)
+                            .setPlural(true)
+                            .setDescription("Item(s) to give"),
+                    new ActionArgument()
+                            .setType(DisplayValue.NUMBER)
+                            .setOptional(true)
+                            .setDescription("Amount to give")
+            )
+                .addAdditionalInfo("This is the first action", "we added!")
+                .setEnchanted(true)
+            .build();
+    }
+
+    @Override
+    public void executeBlockAction(List<Entity> targets, Chest chest) {
+        int multiple = 1;
+        Inventory contents = chest.getBlockInventory();
+        if (contents.contains(Material.SLIME_BALL)){
+            for (ItemStack itemStack : contents){
+                String type = CodeValues.getType(itemStack);
+                if (type != null){
+                    if (type == "num")
+                    {
+                        multiple = Integer.parseInt(CodeValues.getValue(itemStack));
+                        break;
+                    }
+                }
+            }
+        }
+        for (Entity entity : targets){
+            if (entity instanceof Player){
+                Player player = (Player) entity;
+                for (ItemStack item : contents){
+                    if (item != null) {
+                        if (CodeValues.getType(item) == null) {
+                            for (int i = 0; i != multiple; i++) {
+                                player.getInventory().addItem(item);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public String getId() {
+        return "GiveItems";
+    }
+
+    public PlayerActionItems getCategory() {
+        return PlayerActionItems.ITEM_MANAGEMENT_CATEGORY;
+    }
+}

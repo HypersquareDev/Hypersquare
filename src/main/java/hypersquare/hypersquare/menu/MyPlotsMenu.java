38e237737b28827e5ddee55c0109f053b88bb7be
Chickensoup20 <87900686+Chickensoup20@users.noreply.github.com>
2024-01-22 19:34:16 +0200

    refactor: Plot.getPlotItem

--- src/main/java/hypersquare/hypersquare/menu/MyPlotsMenu.java
+++ src/main/java/hypersquare/hypersquare/menu/MyPlotsMenu.java
@@ -4,6 +4,7 @@ import hypersquare.hypersquare.Hypersquare;
 import hypersquare.hypersquare.item.MiscItems;
 import hypersquare.hypersquare.menu.system.Menu;
 import hypersquare.hypersquare.menu.system.MenuItem;
+import hypersquare.hypersquare.plot.Plot;
 import hypersquare.hypersquare.plot.PlotDatabase;
 import hypersquare.hypersquare.util.Utilities;
 import net.kyori.adventure.text.Component;
@@ -38,41 +39,9 @@ public class MyPlotsMenu {
         List<Document> playerPlots = PlotDatabase.getPlotsByOwner(player.getUniqueId().toString());
 
         for (Document plotDocument : playerPlots) {
-            ItemStack plotItem = new ItemStack(Material.matchMaterial(plotDocument.getString("icon")));
-            ItemMeta meta = plotItem.getItemMeta();
-            if (Hypersquare.plotVersion == plotDocument.getInteger("version")) {
-                meta.displayName(cleanMM.deserialize(plotDocument.getString("name")));
-            } else {
-                String name = plotDocument.getString("name");
-                meta.displayName(cleanMM.deserialize(name + "<red>" + " (Out of date)"));
-            }
-            List<Component> lore = new ArrayList<>();
-            lore.add(MiniMessage.miniMessage().deserialize("<dark_gray>" + plotDocument.getString("size") + " Plot").decoration(TextDecoration.ITALIC, false));
-            lore.add(MiniMessage.miniMessage().deserialize(""));
-            lore.add(MiniMessage.miniMessage().deserialize("<gray>Tags: <dark_gray>" + plotDocument.getString("tags")).decoration(TextDecoration.ITALIC, false));
-            lore.add(MiniMessage.miniMessage().deserialize("<gray>Votes: <yellow>" + plotDocument.getInteger("votes") + "<dark_gray> (last 2 weeks)").decoration(TextDecoration.ITALIC, false));
-            lore.add(MiniMessage.miniMessage().deserialize(""));
-            lore.add(MiniMessage.miniMessage().deserialize("<dark_gray>ID: " + plotDocument.getInteger("plotID")).decoration(TextDecoration.ITALIC, false));
-            lore.add(MiniMessage.miniMessage().deserialize("<blue>↓ Node " + plotDocument.getInteger("node")).decoration(TextDecoration.ITALIC, false));
-            if (Hypersquare.plotVersion == plotDocument.getInteger("version")) {
-                lore.add(MiniMessage.miniMessage().deserialize("<dark_gray>Plot version: " + plotDocument.getInteger("version")).decoration(TextDecoration.ITALIC, false));
-            } else {
-                Component aa = MiniMessage.miniMessage().deserialize("<red>Plot version: " + plotDocument.getInteger("version")).decoration(TextDecoration.ITALIC, false);
-                lore.add(aa);
-            }
+            int plotID = plotDocument.getInteger("plotID");
+            ItemStack plotItem = Plot.getPlotItem(plotID);
 
-            meta.lore(lore);
-            meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-            meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-            meta.addItemFlags(ItemFlag.HIDE_DESTROYS);
-            meta.addItemFlags(ItemFlag.HIDE_DYE);
-            meta.addItemFlags(ItemFlag.HIDE_PLACED_ON);
-            meta.addItemFlags(ItemFlag.HIDE_ITEM_SPECIFICS);
-            meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
-            meta.addItemFlags(ItemFlag.HIDE_ARMOR_TRIM);
-            meta.displayName(meta.displayName());
-
-            plotItem.setItemMeta(meta);
             MenuItem plot = new MenuItem(plotItem);
 
             menu.slot(i, plot);

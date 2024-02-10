9737344d4615b85a71dbeebf3f51ef2da39001d8
Chickensoup20 <87900686+Chickensoup20@users.noreply.github.com>
2024-02-10 16:01:43 +0200

    feat: added all missing events*

---
+++ src/main/java/hypersquare/hypersquare/dev/code/player/event/PlayerStartSprintEvent.java
@@ -0,0 +1,44 @@
+package hypersquare.hypersquare.dev.code.player.event;
+
+import hypersquare.hypersquare.item.action.player.PlayerEventItems;
+import hypersquare.hypersquare.item.event.Event;
+import hypersquare.hypersquare.item.event.EventItem;
+import hypersquare.hypersquare.util.color.Colors;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+public class PlayerStartSprintEvent implements Event {
+    @Override
+    public ItemStack item() {
+        return new EventItem()
+            .setMaterial(Material.IRON_BOOTS)
+            .setName(Component.text("Player Start Sprint Event").color(Colors.PURPLE))
+            .setDescription(
+                Component.text("Executes code when a player"),
+                Component.text("starts sprinting."))
+            .setCancellable(true)
+            .build()
+            ;
+    }
+
+    @Override
+    public String getId() {
+        return "start_sprint";
+    }
+
+    @Override
+    public String getCodeblockId() {
+        return "player_event";
+    }
+
+    @Override
+    public String getSignName() {
+        return "StartSprint";
+    }
+
+    @Override
+    public PlayerEventItems getCategory() {
+        return PlayerEventItems.MOVEMENT_EVENTS_CATEGORY;
+    }
+}
---
+++ src/main/java/hypersquare/hypersquare/dev/code/player/event/PlayerSwapHandsEvent.java
@@ -0,0 +1,44 @@
+package hypersquare.hypersquare.dev.code.player.event;
+
+import hypersquare.hypersquare.item.action.player.PlayerEventItems;
+import hypersquare.hypersquare.item.event.Event;
+import hypersquare.hypersquare.item.event.EventItem;
+import hypersquare.hypersquare.util.color.Colors;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+public class PlayerSwapHandsEvent implements Event {
+    @Override
+    public ItemStack item() {
+        return new EventItem()
+            .setMaterial(Material.SHIELD)
+            .setName(Component.text("Player Swap Hands Event").color(Colors.SKY))
+            .setDescription(
+                Component.text("Executes code when a player"),
+                Component.text("swaps items in their offhand and hand."))
+            .setCancellable(true)
+            .build()
+            ;
+    }
+
+    @Override
+    public String getId() {
+        return "swap_hands";
+    }
+
+    @Override
+    public String getCodeblockId() {
+        return "player_event";
+    }
+
+    @Override
+    public String getSignName() {
+        return "SwapHands";
+    }
+
+    @Override
+    public PlayerEventItems getCategory() {
+        return PlayerEventItems.CLICK_EVENTS_CATEGORY;
+    }
+}
